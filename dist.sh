d158c512a34125c3cf5f0ae0c921f9f84eb6091b
Jon Morby <jon@redmail.com>
2023-06-21 16:15:12 +0100

    added chas to the distribution list

--- dist.sh
+++ dist.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd ssl/certificates
-rsync -arv redmail.com.* root@lb-1.server.morby.net:/etc/loadbalancer.org/certs/_.morby.org
-rsync -arv redmail.com.* root@lb-2.server.morby.net:/etc/loadbalancer.org/certs/_.morby.org
-rsync -arv redmail.com.* root@web-1.server.morby.net:/etc/ssl/certificates
+rsync -harvP redmail.com.pem root@lb-1.server.morby.net:/etc/loadbalancer.org/certs/redmail/redmail.pem
+rsync -arvP redmail.com.pem root@lb-2.server.morby.net:/etc/loadbalancer.org/certs/redmail/redmail.pem
+# scp redmail.com.pem root@lb.morby.org:/etc/ssl/redmail/redmail.pem
+rsync -arvP redmail.com.* root@lb-1.server.morby.net:/etc/loadbalancer.org/certs/_.morby.org
+rsync -arvP redmail.com.* root@lb-2.server.morby.net:/etc/loadbalancer.org/certs/_.morby.org
+rsync -arvP redmail.com.* root@web-1.server.morby.net:/etc/ssl/certificates
+rsync -arvP redmail.com.pem redmail.com.issuer.crt root@chas.server.morby.net:/etc/ssl
