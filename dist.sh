96d2001d6d7e9918cb32697c3ce291bd2110f71e
Jon Morby <jon@redmail.com>
2023-11-08 18:03:18 +0000

    more destinations

--- dist.sh
+++ dist.sh
@@ -4,10 +4,14 @@ chmod 0644 redmail.*
 rsync -harvP redmail.com.pem root@lb-1.server.morby.net:/etc/loadbalancer.org/certs/redmail/redmail.pem
 rsync -arvP redmail.com.pem root@lb-2.server.morby.net:/etc/loadbalancer.org/certs/redmail/redmail.pem
 # scp redmail.com.pem root@lb.morby.org:/etc/ssl/redmail/redmail.pem
-rsync -arvP redmail.com.* root@lb-1.server.morby.net:/etc/loadbalancer.org/certs/_.morby.org
-rsync -arvP redmail.com.* root@lb-2.server.morby.net:/etc/loadbalancer.org/certs/_.morby.org
+rsync -arvP redmail.com.pem root@lb-1.server.morby.net:/etc/loadbalancer.org/certs/_.morby.org
+rsync -arvP morby.org.pem root@lb-1.server.morby.net:/etc/loadbalancer.org/certs/_.morby.org
+rsync -arvP redmail.com.pem root@lb-2.server.morby.net:/etc/loadbalancer.org/certs/_.morby.org
+rsync -arvP morby.org.pem root@lb-2.server.morby.net:/etc/loadbalancer.org/certs/_.morby.org
 rsync -arvP redmail.com.* root@web-1.server.morby.net:/etc/ssl/certificates
 rsync -arvP redmail.com.pem redmail.com.issuer.crt root@chas.server.morby.net:/etc/ssl
+rsync -arvP morby.org.pem redmail.com.pem root@devops.eng.morby.net:/etc/ssl/certificates
+rsync -arvP morby.org.pem redmail.com.pem root@192.168.12.50:/etc/ssl/certificates
 
 rsync morby.org.pem root@lb.morby.org:/etc/loadbalancer.org/certs/_.morby.net/_.morby.net.pem
 rsync morby.org.pem root@lb.morby.org:/etc/loadbalancer.org/certs/_.morby.org/_.morby.org.pem
